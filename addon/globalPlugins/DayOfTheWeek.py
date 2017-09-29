5b6d234d788cefce89cac5a7d8a238aad4cc1ae0
abdel792 <abdelkrim.bensaid@gmail.com>
2017-09-29 08:19:08 +0200

    Changed the georgianDays object from tuple to dictionary

--- addon/globalPlugins/DayOfTheWeek.py
+++ addon/globalPlugins/DayOfTheWeek.py
@@ -10,7 +10,7 @@
 #See the file COPYING for more details.
 
 import addonHandler
-addonHandler.initTranslation()
+addonHandler.initTranslation ()
 import globalPluginHandler
 import speech
 import keyboardHandler
@@ -36,18 +36,18 @@ fieldLabels = (
 	_("You can select a year with the vertical arrows")
 )
 
-# The following tuple was created to list the Georgian days that are not recognized by the %A format, this technique will be improved if other languages ??encounter the same problem.
+# The following dictionary was created to list the Georgian days that are not recognized by the %A format, this technique will be improved if other languages ??encounter the same problem.
 # Other localization messages for the Georgian language can be translated by the NVDA translation team.
 
-georgianDays = (
-	u'?????',
-	u'????????',
-	u'?????????',
-	u'?????????',
-	u'?????????',
-	u'?????????',
-	u'??????'
-)
+georgianDays = {
+	"0" : u"კვირა",
+	"1" : u"ორშაბათი",
+	"2" : u"სამშაბათი",
+	"3" : u"ოთხშაბათი",
+	"4" : u"ხუთშაბათი",
+	"5" : u"პარასკევი",
+	"6" : u"შაბათი"
+}
 
 curDateField = 0
 oldSpeechMode = speech.speechMode
@@ -57,53 +57,53 @@ confSpec = {
 	}
 config.conf.spec['dayOfWeek'] = confSpec
 
-class DateDialog(wx.Dialog):
+class DateDialog (wx.Dialog):
 
 	_instance = None
 
-	def __new__(cls, *args, **kwargs):
+	def __new__ (cls, *args, **kwargs):
 		if DateDialog._instance is None:
-			return super(DateDialog, cls).__new__(cls, *args, **kwargs)
+			return super (DateDialog, cls).__new__(cls, *args, **kwargs)
 		return DateDialog._instance
 
-	def __init__(self, parent):
+	def __init__ (self, parent):
 		if DateDialog._instance is not None:
 			return
 		DateDialog._instance = self
 		# Translators: The title of the Date Dialog.
 		dlgTitle = _("Get the day of the week")
-		super(DateDialog,self).__init__(parent,title = dlgTitle)
-		mainSizer = wx.BoxSizer(wx.VERTICAL)
-		sHelper = gui.guiHelper.BoxSizerHelper(self, orientation = wx.VERTICAL)
+		super (DateDialog,self).__init__(parent,title = dlgTitle)
+		mainSizer = wx.BoxSizer (wx.VERTICAL)
+		sHelper = gui.guiHelper.BoxSizerHelper (self, orientation = wx.VERTICAL)
 		# Translators: A label for a list in a dialog.
 		dateLabel = _("Type or select a date")
-		sHelper.addItem(wx.StaticText(self, label = dateLabel))
-		self.datePicker = sHelper.addItem(wx.DatePickerCtrl(self))
-		sHelper.addDialogDismissButtons(self.CreateButtonSizer(wx.OK|wx.CANCEL))
-		self.datePicker.Bind(wx.EVT_CHAR, self.onListChar)
-		self.Bind(wx.EVT_BUTTON, self.onOk, id = wx.ID_OK)
-		mainSizer.Add(sHelper.sizer, border = gui.guiHelper.BORDER_FOR_DIALOGS, flag = wx.ALL)
+		sHelper.addItem (wx.StaticText (self, label = dateLabel))
+		self.datePicker = sHelper.addItem (wx.DatePickerCtrl (self))
+		sHelper.addDialogDismissButtons (self.CreateButtonSizer (wx.OK|wx.CANCEL))
+		self.datePicker.Bind (wx.EVT_CHAR, self.onListChar)
+		self.Bind (wx.EVT_BUTTON, self.onOk, id = wx.ID_OK)
+		mainSizer.Add (sHelper.sizer, border = gui.guiHelper.BORDER_FOR_DIALOGS, flag = wx.ALL)
 		self.Sizer = mainSizer
-		mainSizer.Fit(self)
-		self.datePicker.SetFocus()
-		self.Center(wx.BOTH | wx.CENTER_ON_SCREEN)
+		mainSizer.Fit (self)
+		self.datePicker.SetFocus ()
+		self.Center (wx.BOTH | wx.CENTER_ON_SCREEN)
 
-	def __del__(self):
+	def __del__ (self):
 		DateDialog._instance = None
 
-	def onListChar(self, evt):
+	def onListChar (self, evt):
 		if evt.KeyCode == wx.WXK_SPACE:
 			# Activate the OK button.
-			self.ProcessEvent(wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED, wx.ID_OK))
+			self.ProcessEvent (wx.CommandEvent (wx.wxEVT_COMMAND_BUTTON_CLICKED, wx.ID_OK))
 		else:
-			evt.Skip()
+			evt.Skip ()
 
-	def onOk(self, evt):
+	def onOk (self, evt):
 		import languageHandler
 		curNVDALang = languageHandler.getLanguage ()
-		date = self.datePicker.GetValue()
-		weekDay = date.Format("%A") if not curNVDALang == u"ka" else georgianDays[date.Format ("%w")]
-		msgBox = gui.messageBox(
+		date = self.datePicker.GetValue ()
+		weekDay = date.Format ("%A") if not curNVDALang == u"ka" else georgianDays[date.Format ("%w")]
+		msgBox = gui.messageBox (
 		message = weekDay,
 		# Translators: The title of a dialog.
 		caption = _("Your day"),
@@ -146,8 +146,8 @@ class MyDayOfWeek (IAccessible):
 
 	def whatChanged (self, val1, val2):
 		global curDateField
-		val1 = val1.split("/")
-		val2 = val2.split("/")
+		val1 = val1.split ("/")
+		val2 = val2.split ("/")
 		if val1[0] != val2[0]:
 			curDateField = 1
 		if val1[1] != val2[1]:
@@ -159,52 +159,52 @@ class MyDayOfWeek (IAccessible):
 	def script_nextField (self, gesture):
 		val1 = self.value
 		increment = 0
-		gesture.send()
+		gesture.send ()
 		# I know it is not correct to do this, but we can't do otherwise.
 		speech.speechMode = speech.speechMode_off
-		keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send()
+		keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send ()
 		increment = 1
-		api.processPendingEvents()
+		api.processPendingEvents ()
 		val2 = self.value
 		# We verify that we are not on the last value
 		if val1 == val2:
 			# In this case, we move to the previous value.
-			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send()
+			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send ()
 			increment = -1
-			api.processPendingEvents()
+			api.processPendingEvents ()
 		# We restore the value of the current date.
 		if increment == -1:
-			keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send()
-			api.processPendingEvents()
+			keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send ()
+			api.processPendingEvents ()
 		if increment == 1:
-			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send()
-			api.processPendingEvents()
+			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send ()
+			api.processPendingEvents ()
 		speech.speechMode = oldSpeechMode
 		self.whatChanged (val1, val2)
 
 	def script_previousField (self, gesture):
 		val1 = self.value
 		increment = 0
-		gesture.send()
+		gesture.send ()
 		# I know it is not correct to do this, but we can't do otherwise.
 		speech.speechMode = speech.speechMode_off
-		keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send()
+		keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send ()
 		increment = 1
-		api.processPendingEvents()
+		api.processPendingEvents ()
 		val2 = self.value
 		# We verify that we are not on the last value
 		if val1 == val2:
 			# In this case, we move to the previous value.
-			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send()
+			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send ()
 			increment = -1
-			api.processPendingEvents()
+			api.processPendingEvents ()
 		# We restore the value of the current date.
 		if increment == -1:
-			keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send()
-			api.processPendingEvents()
+			keyboardHandler.KeyboardInputGesture.fromName ("downArrow").send ()
+			api.processPendingEvents ()
 		if increment == 1:
-			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send()
-			api.processPendingEvents()
+			keyboardHandler.KeyboardInputGesture.fromName ("upArrow").send ()
+			api.processPendingEvents ()
 		speech.speechMode = oldSpeechMode
 		self.whatChanged (val1, val2)
 
@@ -213,19 +213,19 @@ class MyDayOfWeek (IAccessible):
 		"kb:rightArrow":"nextField"
 	}
 
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 
 	def __init__(self, *args, **kwargs):
-		super(GlobalPlugin, self).__init__(*args, **kwargs)
-		self.createSubMenu()
+		super (GlobalPlugin, self).__init__(*args, **kwargs)
+		self.createSubMenu ()
 
 	def chooseNVDAObjectOverlayClasses (self, obj, clsList):
-		if obj.value and obj.role == controlTypes.ROLE_DROPLIST and len(obj.value) == 10 and "/" in obj.value:
+		if obj.value and obj.role == controlTypes.ROLE_DROPLIST and len (obj.value) == 10 and "/" in obj.value:
 			clsList.insert (0, MyDayOfWeek)
 
-	def createSubMenu(self):
+	def createSubMenu (self):
 		self.menu = gui.mainFrame.sysTrayIcon.preferencesMenu
-		dowMenu = wx.Menu()
+		dowMenu = wx.Menu ()
 		self.mainItem = self.menu.AppendSubMenu (dowMenu,
 		# Translators: Item in the preferences menu for the Addon dayOfTheWeek.
 		_("&Day of the week..."),
@@ -236,41 +236,41 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		_("Search a &day"),
 		# Translators: The tooltyp text for the first item in the dayOfTheWeek add-on submenu.
 		_("Search a day in the calendar"))
-		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onDateDialog, dateChoice)
+		gui.mainFrame.sysTrayIcon.Bind (wx.EVT_MENU, self.onDateDialog, dateChoice)
 		addonSettings = dowMenu.Append (wx.ID_ANY,
 		# Translators: The name of the second item in the dayOfTheWeek add-on submenu.
 		_("dayOfTheWeek add-on se&ttings"),
 		# Translators: The tooltyp text for the second item in the dayOfTheWeek add-on submenu.
 		_("Configure the dayOfTheWeek add-on"))
-		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onAddonSettingsDialog, addonSettings)
+		gui.mainFrame.sysTrayIcon.Bind (wx.EVT_MENU, self.onAddonSettingsDialog, addonSettings)
 
-	def terminate(self):
+	def terminate (self):
 		try:
-			self.menu.RemoveItem(self.mainItem)
+			self.menu.RemoveItem (self.mainItem)
 		except wx.PyDeadObjectError:
 			pass
 
 	def onDateDialog (self, evt):
-		gui.mainFrame.prePopup()
+		gui.mainFrame.prePopup ()
 		d = DateDialog (gui.mainFrame)
-		d.Show()
-		gui.mainFrame.postPopup()
+		d.Show ()
+		gui.mainFrame.postPopup ()
 
 	def onAddonSettingsDialog (self, evt):
-		gui.mainFrame.prePopup()
+		gui.mainFrame.prePopup ()
 		d = DayOfWeekSettingsDialog (gui.mainFrame)
-		d.Show()
-		gui.mainFrame.postPopup()
+		d.Show ()
+		gui.mainFrame.postPopup ()
 
-	def script_activateDayOfTheWeekDialog(self, gesture):
-		wx.CallAfter(self.onDateDialog, gui.mainFrame)
+	def script_activateDayOfTheWeekDialog (self, gesture):
+		wx.CallAfter (self.onDateDialog, gui.mainFrame)
 
 	# Translators: Message presented in input help mode.
 	script_activateDayOfTheWeekDialog.__doc__ = _("Allows you to find the day of the week corresponding to a chosen date.")
 	script_activateDayOfTheWeekDialog.category = SCRCAT_TOOLS
 
-	def script_activateDayOfTheWeekSettingsDialog(self, gesture):
-		wx.CallAfter(self.onAddonSettingsDialog, gui.mainFrame)
+	def script_activateDayOfTheWeekSettingsDialog (self, gesture):
+		wx.CallAfter (self.onAddonSettingsDialog, gui.mainFrame)
 
 	# Translators: Message presented in input help mode.
 	script_activateDayOfTheWeekSettingsDialog.__doc__ = _("Allows you to open the dayOfTheWeek add-on settings dialog.")
