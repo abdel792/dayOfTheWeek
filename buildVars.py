78a688a8c4582b4930f95f7d722c71ad141db95d
abdel792 <abdelkrim.bensaid@gmail.com>
2023-05-07 15:43:13 +0200

    Created the version 23.05 including compatibility with nvda-2023.1 as well as L10N updates

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""With this add-on, you'll be able to find the day of the week corresponding to a chosen date."""),
 	# version
-	"addon_version": "22.06",
+	"addon_version": "23.05",
 	# Author(s)
 	"addon_author": "Abdel <abdelkrim.bensaid@gmail.com>, Noelia <nrm1977@gmail.com>",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion": "2014.3",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2022.1",
+	"addon_lastTestedNVDAVersion": "2023.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel": None,
 }
